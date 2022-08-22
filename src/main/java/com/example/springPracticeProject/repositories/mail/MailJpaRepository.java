6200fb20118cb89a1068cb647c58d3dbc3285b59
na1x <na1x.1024@gmail.com>
2022-08-22 09:48:43 +0300

    mini fix

--- src/main/java/com/example/springPracticeProject/repositories/mail/MailJpaRepository.java
+++ src/main/java/com/example/springPracticeProject/repositories/mail/MailJpaRepository.java
@@ -7,6 +7,6 @@ public interface MailJpaRepository extends JpaRepository<Mail, Long> {
 
     Mail getById(Long id);
     Mail findByUserId(Long userId);
-
+//    Mail findTopByMailAddress(String mailAddress);
     Mail findByMailAddress(String mailAddress);
 }
